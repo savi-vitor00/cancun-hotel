e54f6e32ef488870765bc6656dbad2c9db8d37ff
savi-vitor00 <savi.vitor00@gmail.com>
2022-07-08 08:47:39 +0100

    first commit to create repo

---
+++ src/main/java/com/cancun/hotel/cancunhotel/CancunHotelApplication.java
@@ -0,0 +1,13 @@
+package com.cancun.hotel.cancunhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancunHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancunHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cancun/hotel/cancunhotel/CancunHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cancun.hotel.cancunhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancunHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
